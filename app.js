dfa7f2655845b2c8107767cba86e6a5c2dd13272
Luiseeop <luiseeop1@gmail.com>
2025-05-19 17:03:45 +0200

    nex

--- app.js
+++ app.js
@@ -3,23 +3,22 @@ fetch('https://fakestoreapi.com/products')
   .then(data => {
    
     
-    const container = document.getElementById('cartes-container');
+    
 
     data.forEach(produit => {
-      const carte = document.createElement('div');
-      carte.classList.add('div');
 
-      carte.innerHTML = `
-        <img src="${produit.image}" alt="${produit.title}" class="img">
+      document.getElementById("cartes-container").innerHTML += `
+        
+      <div class="div">
+      <img src="${produit.image}" alt="${produit.title}" class="img">
         <div class="p10">
           <h2>${produit.title}</h2>
           <p class="size20">${produit.description}</p>
           <p class="prix">${produit.price} €</p>
         </div>
         <div class="btn">Ajouter au panier</div>
+        </div>
       `;
-
-      container.appendChild(carte);
     });
 
 
